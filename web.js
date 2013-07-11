5eed8a9fc1f46fb839b980e046cf206a88fac3c6
wmavis <will@covertapps.com>
2013-07-11 02:42:58 +0000

    Initialize fs

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   content = fs.readFileSync("index.html").toString();
   response.send(content);
 });
