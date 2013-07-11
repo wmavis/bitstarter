56adffe75af77cad630e14bf605a459fa0eb85a7
wmavis <will@covertapps.com>
2013-07-11 02:40:25 +0000

    Update web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  content = fs.readFileSync("index.html").toString();
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
